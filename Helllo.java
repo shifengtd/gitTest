b28a7fbaa99c28362c01c4ab17017708d5b13884
shifengtd <49134298+shifengtd@users.noreply.github.com>
2019-04-01 09:47:35 +0800

    修改后的代码
    增加输出语句

--- Helllo.java
+++ Helllo.java
@@ -2,4 +2,6 @@ public class Hello{
    public static void main(String [] arg)
       
       System.out.println("Hello world !")
+      System.out.println("Hello world222 !")
+      System.out.println("Hello world333 !")
 }
