a41c429975ec5d0e699516bb4a1ba73e11d19851
shifengtd <49134298+shifengtd@users.noreply.github.com>
2019-04-01 09:48:58 +0800

    删除代码
    测试删除代码效果

--- Helllo.java
+++
@@ -1,7 +0,0 @@
-public class Hello{
-   public static void main(String [] arg)
-      
-      System.out.println("Hello world !")
-      System.out.println("Hello world222 !")
-      System.out.println("Hello world333 !")
-}
